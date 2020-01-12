3df9b11e50fde1d4de032e40c91c5f55f83b1861
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2020-01-12 21:25:56 -0200

    set header title

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 export default class Main extends Component {
+    static navigationOptions = {
+        title: 'JS Hunt'
+    }
+
     render() {
         return (
             <View>
