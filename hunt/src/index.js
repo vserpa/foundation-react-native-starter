5af2fd8b953f34f1440b7e6558e3122888d5aab1
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2020-01-12 21:21:04 -0200

    create index file

---
+++ hunt/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+
+// Class format of a Component
+// class App extends Component {
+//     render() {
+//         return <Routes />
+//     }
+// }
+
+// Function format of a Component
+const App = () => <Routes />
+
+export default App;
