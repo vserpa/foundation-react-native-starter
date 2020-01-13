2418ad23aafd2980f8a7df09e45e8f26ee311438
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2020-01-12 22:53:57 -0200

    list data with FlatList

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import api from '../services/api';
 
 export default class Main extends Component {
@@ -23,13 +23,24 @@ export default class Main extends Component {
         this.setState({ docs });
     }
 
+    renderItem = ({ item }) => (
+        <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+            <TouchableOpacity onPress={() => {}}>
+                <Text>Access</Text>
+            </TouchableOpacity>
+        </View>
+    )
+
     render() {
         return (
             <View>
-                <Text>Main Page</Text>
-                {this.state.docs.map(product => (
-                    <Text key={product._id}>{product.title}</Text>
-                ))}
+                <FlatList
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
             </View>
         )
     }
