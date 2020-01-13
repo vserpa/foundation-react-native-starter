de871c7fc616a83508df19c64054e91b03d478a3
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2020-01-12 22:44:12 -0200

    use state to get returned data

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -7,6 +7,10 @@ export default class Main extends Component {
         title: 'JS Hunt'
     };
 
+    state = {
+        docs: []
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
@@ -15,14 +19,17 @@ export default class Main extends Component {
     // se utiliza do escopo corrente e possui acesso à this.*
     loadProducts = async () => {
         const response = await api.get('/products');
-        const { docs } = response.data;
-        console.log(docs);
+        const { docs } = response.data;        
+        this.setState({ docs });
     }
 
     render() {
         return (
             <View>
                 <Text>Main Page</Text>
+                {this.state.docs.map(product => (
+                    <Text key={product._id}>{product.title}</Text>
+                ))}
             </View>
         )
     }
