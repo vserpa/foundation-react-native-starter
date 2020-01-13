7a6d82a17294f5da5d91366f9af86a0b9488c626
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2020-01-12 22:20:40 -0200

    consume the api an show data for debug

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
     static navigationOptions = {
         title: 'JS Hunt'
+    };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    // arrow function nunca cria um novo escopo de função, ele 
+    // se utiliza do escopo corrente e possui acesso à this.*
+    loadProducts = async () => {
+        const response = await api.get('/products');
+        const { docs } = response.data;
+        console.log(docs);
     }
 
     render() {
